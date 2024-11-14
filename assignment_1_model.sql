fc3c2249096b1698b8e51940d41cb5a928f71f5e
Timothy <timothycassel@gmail.com>
2024-11-14 17:40:36 +0100

    [feat]: Added the JSON tables to the Data Model

--- assignment_1_model.sql
+++ assignment_1_model.sql
@@ -11,6 +11,10 @@ DROP TABLE IF EXISTS UnscheduledMaintenanceEvent CASCADE;
 DROP TABLE IF EXISTS WorkOrderAOS CASCADE;
 DROP TABLE IF EXISTS WorkOrderOI CASCADE;
 DROP TABLE IF EXISTS Trip CASCADE;
+DROP TABLE IF EXISTS CustomerPreference CASCADE; 
+DROP TABLE IF EXISTS AircraftMaintenanceLogs CASCADE; 
+DROP TABLE IF EXISTS CustomerFeedback CASCADE; 
+
 
 ----------------- Task 2: Create Tables of the database -----------------
 
@@ -150,4 +154,28 @@ CREATE TABLE IF NOT EXISTS WorkOrderOI (
     required_parts VARCHAR(50),
     estimated_completion_time INT,
     status VARCHAR(20) CHECK (status IN ('in-progress', 'pending', 'completed')) NOT NULL  
+);
+
+
+-- Customer Preferences Table 
+CREATE TABLE IF NOT EXISTS CustomerPreference (
+ 	customer_preference_id INT PRIMARY KEY,
+ 	customer_id INT REFERENCES Customer(customer_id) ON DELETE CASCADE,
+ 	preferences JSONB
+);
+
+
+-- Aircraft Maintenace Logs Table 
+CREATE TABLE IF NOT EXISTS AircraftMaintenanceLogs (
+	maintenance_event_log_id INT PRIMARY KEY,
+	maintenance_event_id INT REFERENCES MaintenanceEvent(maintenance_event_id) ON DELETE CASCADE,
+	maintenance_log JSONB
+	
+);
+
+-- Customer Feedback Table 
+CREATE TABLE IF NOT EXISTS CustomerFeedback (
+	feedback_id INT PRIMARY KEY,
+	customer_id INT REFERENCES Customer(customer_id) ON DELETE CASCADE,
+	survey JSONB
 );
